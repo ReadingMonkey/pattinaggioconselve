3a528b0c7800305fc77e0f917a5c423efc8b21a9
ReadingMonkey <91528923+ReadingMonkey@users.noreply.github.com>
2026-09-18 14:51:28 +0200

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
+    // Inserisci l'URL principale di distribuzione Apps Script (/exec)
+    const appUrl = "https://script.google.com/macros/s/1PD1VC6hOVAvo3lRj1niB9fs89hiNLoM5jNn684ToU65Kv20UVQD5oZKD/exec";
 
-    $.ajax({
-        url: "https://script.googleusercontent.com/macros/echo?user_content_key=0kPhTGmLzcBLHcTxYhR0rylEZc3oTe48Enb4q7GesuPdSGhVcc_tROsLUOidX4G399d_1nBN8c4To0oBh0UncMD7Z3USVQnXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDgOW764LuaOWFEh3hyKkyy57_Z0kBb9tRGHEtCW_LGBvEtKhAQnnv3kJETbalFVXSutrfMuyT7DGCd7RqHVt6odfQTHyigprdz9Jw9Md8uu&lib=MOGp-nxa3Cm_dWFjptXP0VnAnurRqelap",
-        success: function(data){
-            for(let i=0; i<data.length; i++){
+    fetch(appUrl)
+        .then(response => {
+            if (!response.ok) throw new Error("Errore nella risposta");
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(item => {
                 $('#palmares').append(`
                     <div class="media d-flex mb-3">
                         <div class="media-body">
-                        <h5 class="pt-2">${data[i]['Titolo']}</h5>
-                        ${data[i]['Sottotitolo']}
+                            <h5 class="pt-2">${item['Titolo']}</h5>
+                            ${item['Sottotitolo']}
                         </div>
                     </div>
                 `);
-            }
-        }
-    });
-
+            });
+        })
+        .catch(err => console.error("Errore CORS o di rete:", err));
 });
